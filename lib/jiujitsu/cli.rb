a3e92ed8ac096290566e92261fdca9e17dcdfabc
Nash Brancic <nashfft@gmail.com>
2019-04-12 06:00:24 +0000

    making methods work

--- lib/jiujitsu/cli.rb
+++ lib/jiujitsu/cli.rb
@@ -3,16 +3,27 @@ class Jiujitsu::CLI
 
   def start
     puts "Welcome to Venum"
+    menu
+  end
+
+  def menu
     puts "What type of equipment are you interested in?"
-    puts "BJJ, Boxing, Karate"
+    puts "BJJ GI's, Boxing Gloves, Karate GI's"
     puts "Type either 'bjj', 'boxing', or 'karate'"
     input = gets.strip.downcase
     case input
     when "bjj"
-        puts "in bjj"
-        url = "https://www.venum.com/sports/bjj/bjj-gis.html"
-        types = Jiujitsu::Scraper.scrape_equipment(url)
-        puts types[1]
+        puts "in GI's"
+        scrape_gis
+        list_gis
+        chose_your_gi
+        # puts "Chose another GI you would like to see"
+        # # list more options
+        # types.each.with_index(1) do |color, index|
+        #   puts "#{index}. #{color.name}"
+        # end
+        # puts types[1].name
+        # puts types[1].url
       when "boxing"
         puts "in boxing"
       when "karate"
@@ -21,6 +32,38 @@ class Jiujitsu::CLI
         puts "Goodbye"
       else
         puts "no such option"
+        menu
     end
   end
-end
+
+  def list_gis
+    Jiujitsu::Style.all.each.with_index(1) do |color, index|
+      puts "#{index}. #{color.name}"
+    end
+  end
+
+  def chose_your_gi
+    puts "Select another GI with the correct number:"
+    input = gets.strip.to_i
+    total = Jiujitsu::Style.all.length
+    if input.between?(1,total)
+      type = Jiujitsu::Style.all[input-1]
+      show_the_gi(input)
+      # okay
+    else
+      puts "\nPlease select the correct GI"
+      list_gis
+      chose_your_gi
+    end
+  end
+
+  def show_the_gi(type)
+
+  end
+
+
+  def scrape_gis
+    url = "https://www.venum.com/sports/bjj/bjj-gis.html"
+    types = Jiujitsu::Scraper.scrape_equipment(url)
+    end
+  end
--- lib/jiujitsu/scraper.rb
+++ lib/jiujitsu/scraper.rb
@@ -2,6 +2,8 @@ class Jiujitsu::Scraper
 
   def self.scrape_equipment(url)
     website = Nokogiri::HTML(open(url))
+    #binding.pry
+    #light = website.css("h1.page-title")#.text.gsub("\n","") #Gets categori name
     light = website.css("div.products.wrapper.grid")
     array_of_gis = light.css("a.product-item-link")
 
