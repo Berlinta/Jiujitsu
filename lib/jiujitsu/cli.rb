484a8a565a27e5278fd48ed3d1d405f9d6615d46
Nash Brancic <nashfft@gmail.com>
2019-04-16 05:04:54 +0000

    First CLI done

--- lib/jiujitsu/cli.rb
+++ lib/jiujitsu/cli.rb
@@ -10,47 +10,68 @@ class Jiujitsu::CLI
 
   def menu
     puts "Quality Brazilian Jiu Jitsu GI's"
-    puts "Type all to see the list available of GI's"
-    puts "Type close to exit"
+    puts "Type ALL to see the list available of GI's"
+    puts "Type CLOSE to exit"
     input = gets.strip.downcase
     case input
-    when "all"
-        puts "Select the GI to get the price"
+      when "all"
+        puts "Select the GI to get the price tag"
         scrape_gis
         list_gis
         chose_your_gi
         show_price
+        other
       when "close"
         puts "Thank you for checking out our selection."
       else
         puts "INVALID SELECTION"
-        menu
     end
   end
 
-  def list_gis
-    Jiujitsu::Style.all.each.with_index(1) do |color, index|
+  def other
+    puts "Type BACK to return to the menu"
+    puts "Type OTHER to get check another model"
+    input = gets.strip.downcase
+    case input
+    when "back"
+      logo
+      menu
+    when "other"
+      scrape_gis
+      list_gis
+      chose_your_gi
+      show_price
+      other
+    when "close"
+      puts "Thank you for checking out our selection."
+    else
+      puts "INVALID SELECTION"
+    end
+  end
+
+  def list_gis #LIST ALL THE MODELS
+    Jiujitsu::Style.all.take(10).each.with_index(1) do |color, index|
       puts "#{index}. #{color.name}"
     end
   end
 
-  def chose_your_gi
-    puts "Select another GI with the correct number:"
+  def chose_your_gi #PICK A SPECIFIC MODEL
+    puts "Select the GI with the correct number:"
     input = gets.strip.to_i
     @gi = Jiujitsu::Style.all[input-1]
     #binding.pry
   end
 
-  def get_price
+  def get_price #GET THE PRICE
     Jiujitsu::Scraper.scrape_price(self.gi)
   end
 
-  def show_price
-    puts "This is the price for this model: #{get_price}"
+  def show_price #SHOW PRICE
+    puts "The price for this model is: #{get_price}"
     #binding.pry
   end
 
-  def show_the_gi(type)
+  def show_the_gi(type) #SHOW THE MODEL
     Jiujitsu::Scraper.scrape_type(type)
   end
 
@@ -68,5 +89,4 @@ class Jiujitsu::CLI
   ## ##   ##       ##   ### ##     ## ##     ##
    ###    ######## ##    ##  #######  ##     ## "
  end
-
 end
--- lib/jiujitsu/scraper.rb
+++ lib/jiujitsu/scraper.rb
@@ -1,12 +1,12 @@
 class Jiujitsu::Scraper
 
-  def self.scrape_equipment(url) #SHOW ALL THE AVAILABLE GIs
+  def self.scrape_equipment(url) #SCRAPES GIs
     website = Nokogiri::HTML(open(url))
     light = website.css("div.products.wrapper.grid")
     array_of_gis = light.css("a.product-item-link")
-    array_of_gis[0..9].each do |gi|
+    array_of_gis[0..10].each do |gi|
       text = gi.text
-      url = gi.attributes["href"].value#.split("html")[1] + "html"
+      url = gi.attributes["href"].value         #.split("html")[1] + "html"
       Jiujitsu::Style.new(text, url)
     end
   end
@@ -14,6 +14,5 @@ class Jiujitsu::Scraper
  def self.scrape_price(obj) #SCRAPES THE PRICE
     website = Nokogiri::HTML(open(obj.url))
     obj.price = website.css(".price").first.text
-    #binding.pry
  end
 end
